1676589de473470f1629e6f34f1daf3d7ec707ea
Macktireh <abdimack97@gmail.com>
2024-04-12 22:46:23 +0200

    refactoring of the base and final application.

--- CustomTkinter/demo-final-app/components/taskCounter.py
+++ CustomTkinter/demo-final-app/components/taskCounter.py
@@ -1,12 +1,14 @@
+from typing import List
 from customtkinter import CTkBaseClass, CTkLabel, StringVar
 
 from utils import count_tasks_not_completed
+from utils.types import Task
 
 
 class TaskCounter(CTkLabel):
-    def __init__(self, master: CTkBaseClass) -> None:
+    def __init__(self, master: CTkBaseClass, tasks: List[Task]) -> None:
         self.master = master
-        self.count = count_tasks_not_completed(self.master.tasks)
+        self.count = count_tasks_not_completed(tasks)
         self.text_label = StringVar(value=f"Tâches à faire : {self.count}")
 
         super().__init__(
